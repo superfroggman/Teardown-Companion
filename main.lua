189db9074160d09c84a990fc8f2ca132be203fd1
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-08-18 15:07:09 +0200

    commented code and removed debug prints

--- main.lua
+++ main.lua
@@ -19,25 +19,24 @@ function tick()
 	table.remove(playerPositions, 1)
     table.insert(playerPositions, GetPlayerTransform().pos)
 
-	DebugPrint(VecLength(VecSub(playerPositions[1], playerPositions[stepAmount])))
-
+	--Get amount of steps in wrong direction
 	local amountWrong = 0
 	for i = 1, stepAmount, 1 do
 		if (VecLength(VecSub(playerPositions[i], playerPositions[stepAmount])) > VecLength(VecSub(pos, playerPositions[stepAmount]))) then
-			DebugPrint("LONG" .. GetTime())
 			amountWrong = amountWrong + 1
 		else
 			break
 		end
 	end
 
+	--Smooth movement if it would otherwise move in wrong direction
 	if(amountWrong == 0) then
 		pos = playerPositions[1]
 	else
 		pos = VecAdd(pos, VecScale(VecSub(playerPositions[amountWrong+1],pos), 1/amountWrong))
 	end
 	
-
+	--Draw sprite
 	local t = Transform(VecAdd(pos, Vec(0, 0.5, 0)),  GetPlayerTransform().rot)
 	DrawSprite(sprite, t, 2, 2, 1, 1, 1, 1, true)
 
