b993b8ee23ac27d24b9ed7935909917a97280960
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-08-18 15:04:55 +0200

    added new following movement

--- main.lua
+++ main.lua
@@ -1,21 +1,45 @@
 local sprite
 local pos
+
+local playerPositions = {}
+
+local stepAmount = 300
+
 function init()
 	sprite = LoadSprite("dog.png")
 	pos = GetPlayerTransform().pos
+
+	for i = 1, stepAmount, 1 do
+		playerPositions[i] = pos
+	end
 end
 
 function tick()
-	local playerPos = GetPlayerTransform().pos
-	local posDiff = VecSub(playerPos, pos)
-	local posChange = VecScale(posDiff, 1/VecLength(posDiff))
+	--Shift array
+	table.remove(playerPositions, 1)
+    table.insert(playerPositions, GetPlayerTransform().pos)
 
-	pos = VecAdd(pos, VecScale(posDiff, 0.01))
-	DebugPrint(pos[1])
+	DebugPrint(VecLength(VecSub(playerPositions[1], playerPositions[stepAmount])))
 
-	local t = Transform(VecAdd(pos, Vec(0, 0.5, 0)),  GetCameraTransform().rot)
-	DebugPrint(GetPlayerTransform().rot[2])
-	DrawSprite(sprite, t, 2, 2, 1, 1, 1, 1)
+	local amountWrong = 0
+	for i = 1, stepAmount, 1 do
+		if (VecLength(VecSub(playerPositions[i], playerPositions[stepAmount])) > VecLength(VecSub(pos, playerPositions[stepAmount]))) then
+			DebugPrint("LONG" .. GetTime())
+			amountWrong = amountWrong + 1
+		else
+			break
+		end
+	end
+
+	if(amountWrong == 0) then
+		pos = playerPositions[1]
+	else
+		pos = VecAdd(pos, VecScale(VecSub(playerPositions[amountWrong+1],pos), 1/amountWrong))
+	end
+	
+
+	local t = Transform(VecAdd(pos, Vec(0, 0.5, 0)),  GetPlayerTransform().rot)
+	DrawSprite(sprite, t, 2, 2, 1, 1, 1, 1, true)
 
 end
 
